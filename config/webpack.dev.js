010457faa900eff5d2d1590c683870a986b24a13
wyp <593704361@qq.com>
2018-09-11 16:41:13 +0800

    *

--- config/webpack.dev.js
+++
@@ -1,8 +0,0 @@
-const merge = require('webpack-merge')
-
-const base = require('./webpack.base.js')
-
-module.exports = merge(base, {
-    devtool: 'cheap-module-eval-source-map',
-    mode: 'development'
-})
